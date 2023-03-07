f611151bd408272e43b73f7eab0dbeae13888cf9
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-03-07 18:11:19 +0100

    Add Pie and Catogory repositories

---
+++ BethanysPieShop/Models/PieRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models;
+
+public class PieRepository : IPieRepository
+{
+    private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+
+    public PieRepository(BethanysPieShopDbContext bethanysPieShopDbContext)
+    {
+        _bethanysPieShopDbContext = bethanysPieShopDbContext;
+    }
+
+    public IEnumerable<Pie> AllPies => _bethanysPieShopDbContext.Pies.Include(p => p.Category);
+
+    public IEnumerable<Pie> PiesOfTheWeek => _bethanysPieShopDbContext.Pies.Include(p => p.Category).Where(p => p.IsPieOfTheWeek == true);
+
+    public Pie? GetPieById(int id) => _bethanysPieShopDbContext.Pies.FirstOrDefault(p => p.PieId == id);
+}
