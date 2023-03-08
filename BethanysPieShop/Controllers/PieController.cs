31f5fdabb927b71c3a6e12b718f1bfea3e4c55d0
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-03-08 09:30:51 +0100

    Add Home

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -20,8 +20,18 @@ public class PieController : Controller
         //ViewBag.CurrentCategory = "Cheese Cakes";
         //return View(_pieRepository.AllPies);
         PieListViewModel pieListViewModel = new PieListViewModel
-            (_pieRepository.AllPies, "Cheese cakes");
+            (_pieRepository.AllPies, "All pies");
         return View(pieListViewModel);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var pie = _pieRepository.GetPieById(id);
+        if(pie == null)
+        {
+            return NotFound();
+        }
+        return View(pie);
     }
 
 }
